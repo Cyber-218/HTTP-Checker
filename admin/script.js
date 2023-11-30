b0db478f245cebd0dd515f6e520aad9bdac0394d
Cyber 218 <148788983+Cyber-218@users.noreply.github.com>
2023-11-30 08:01:42 +0200

    Create script.js

---
+++ admin/script.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function () {
+    Swal.fire({
+        title: '😄 Nothing to Hack!',
+        text: 'This is just a friendly notification.',
+        icon: '😄',
+        confirmButtonText: 'Okay',
+        allowOutsideClick: false,
+        allowEscapeKey: false,
+        allowEnterKey: false,
+        showCloseButton: false,
+        showCancelButton: false,
+    });
+});
